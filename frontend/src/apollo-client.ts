d79f32a001bb3d68268e596292997f3bf0ee3ae2
MattElcock <mattiose@outlook.com>
2022-12-11 15:50:19 +0000

    pass URL in through env

--- frontend/src/apollo-client.ts
+++ frontend/src/apollo-client.ts
@@ -1,8 +1,11 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache } from '@apollo/client'
+import getConfig from 'next/config'
+
+const { publicRuntimeConfig } = getConfig()
 
 const client = new ApolloClient({
-  uri: "http://localhost:1337/graphql",
+  uri: publicRuntimeConfig.databaseURI,
   cache: new InMemoryCache(),
-});
+})
 
-export { client };
+export { client }
