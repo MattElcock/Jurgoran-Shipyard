ed4b4227c9e371262475381ce933eb9e51ca6c11
MattElcock <mattiose@outlook.com>
2022-12-21 20:22:28 +0000

    chore: add analytics

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,16 +4,20 @@ import { AppLayout } from 'layouts/AppLayout'
 import { ShoppingCartProvider } from 'providers/ShoppingCartProvider/ShoppingCartProvider'
 import theme from 'theme'
 import 'styles/global.css'
+import { Analytics } from '@vercel/analytics/react'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <ShoppingCartProvider>
-        <AppLayout>
-          <Component {...pageProps} />
-        </AppLayout>
-      </ShoppingCartProvider>
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={theme}>
+        <ShoppingCartProvider>
+          <AppLayout>
+            <Component {...pageProps} />
+          </AppLayout>
+        </ShoppingCartProvider>
+      </ChakraProvider>
+      <Analytics />
+    </>
   )
 }
 
